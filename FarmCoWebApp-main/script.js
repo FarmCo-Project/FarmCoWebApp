66d235bf19e4ba656d7cdb274f1ff0b414573f01
Stacy Njoki <124655887+njokistacy@users.noreply.github.com>
2025-06-09 06:09:42 +0300

    Commit of few changes & additional files/pages
    Added logo to the login page and additional " My Products" page (not fully completed) to the FarMaCo version released by Brian Sanaet

---
+++ FarmCoWebApp-main/script.js
@@ -0,0 +1,9 @@
+<form onsubmit="handleLogin(event)">
+  <label for="role">Select Role:</label>
+  <select id="role" required>
+    <option value="">--Choose Role--</option>
+    <option value="farmer">Farmer</option>
+    <option value="customer">Customer</option>
+  </select>
+  <button type="submit">Login</button>
+</form>
